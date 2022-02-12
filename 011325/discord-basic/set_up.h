223b1683700847692fc550580bd16085009f72bd
Andy8787(布長) <andy8787main@gmail.com>
2022-02-12 15:33:43 +0800

    discord-basic

---
+++ 011325/discord-basic/set_up.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <fstream> //讀檔案
+#include <stdlib.h> //rand
+#include <time.h> //rand
+
+#define datasize 5
+#define welcomesize 6
+#define ShowAllDataAndWelcome false
+using std::cout;
+using std::cin;
+using std::string;
+using std::ifstream;
+using std::endl;
